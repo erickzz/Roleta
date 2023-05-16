3de08e1fd5df154d731766e0e8570b5859028373
erickzz <erick.reis2@gmail.com>
2023-05-16 09:16:12 -0300

    basename

--- src/index.js
+++ src/index.js
@@ -12,12 +12,15 @@ import App from './pages/App';
 import RespostaCerta from './pages/RespostaCerta';
 import RespostaErrada from './pages/RespostaErrada';
 
-const router = createHashRouter([
-  { path: '/Roleta', element: <App /> },
-  { path: '/:tema', element: <PaginaPerguntas /> },
-  { path: '/correct', element: <RespostaCerta /> },
-  { path: '/incorrect', element: <RespostaErrada /> },
-]);
+const router = createHashRouter(
+  [
+    { path: '/Roleta', element: <App /> },
+    { path: '/:tema', element: <PaginaPerguntas /> },
+    { path: '/correct', element: <RespostaCerta /> },
+    { path: '/incorrect', element: <RespostaErrada /> },
+  ],
+  { basename: '/Roleta' }
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
