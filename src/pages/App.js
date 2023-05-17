d94ecaa6c32c0d76753ab404893b185f5ea4f813
erickreis <erick.reis2@gmail.com>
2023-05-17 16:36:11 -0300

    Altertacao Educampo

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import WheelComponent from '../components/wheel';
 import '../index.css';
-import LogoVarejo from '../assets/Logo_Varejo.png';
-import LogoFocus from '../assets/Logo_Focus.png';
+//import LogoVarejo from '../assets/Logo_Varejo.png';
+//import LogoFocus from '../assets/Logo_Focus.png';
 import ClickIcon from '../assets/Click_Icon.jpeg';
+import MarcaSebrae from '../assets/marca-sebrae.png';
+import LogoParque from '../assets/Logo_Parque.png';
 
 import { useNavigate } from 'react-router-dom';
 
 const App = () => {
   const navigate = useNavigate();
 
-  const segments = ['Varejo', 'Logística', 'Operacional'];
-  const segColors = ['#faa230', '#055cba', '#bb1625'];
+  const segments = ['Inova 1', 'Inova 2', 'Inova 3'];
+  const segColors = ['#045bbc', '#04949c', '#5c7c2c'];
   const onFinished = (winner) => {
     //console.log(winner);
     setTimeout(() => {
@@ -26,7 +28,7 @@ const App = () => {
   return (
     <>
       <div className="main_text">
-        <p>Varejo+ Experience</p>
+        <p>Quiz Educampo</p>
       </div>
 
       <img className="icone-click" src={ClickIcon} alt="Ícone de Clique" />
@@ -45,10 +47,10 @@ const App = () => {
         fontFamily="Arial"
       />
       <div className="left_img">
-        <img src={LogoVarejo} alt="Varejo+ Logo" />
+        <img src={LogoParque} alt="Logo Parque Científico Tecnológico" />
       </div>
       <div className="right_img">
-        <img src={LogoFocus} alt="Focus Logo" />
+        <img src={MarcaSebrae} alt="Logo Sebrae" />
       </div>
     </>
   );
--- src/perguntas.js
+++ src/perguntas.js
@@ -1,135 +1,553 @@
 export const Perguntas = [
   {
-    tema: 'Operacional',
-    pergunta:
-      'Em 2021, qual foi a participação do e-commerce no faturamento do comércio varejista restrito?',
-    alternativas: [
-      { texto: '12,2%', valor: true },
-      { texto: '02,2%', valor: false },
-      { texto: '22,9%', valor: false },
-      { texto: '38,2%', valor: false },
-    ],
-  },
-  {
-    tema: 'Varejo',
-    pergunta: 'Qual a taxa de crescimento do e-commerce no ano de 2021?',
-    alternativas: [
-      { texto: '78,5%', valor: true },
-      { texto: '88,5%', valor: false },
-      { texto: '70%', valor: false },
-      { texto: '65,5%', valor: false },
-    ],
-  },
-  {
-    tema: 'Logística',
-    pergunta: 'Qual foi o primeiro e-commerce do Brasil?',
-    alternativas: [
-      { texto: 'Magazine Luiza', valor: true },
-      { texto: 'Carrefour', valor: false },
-      { texto: 'Mercado Livre', valor: false },
-      { texto: 'Casas Bahia', valor: false },
-    ],
-  },
-  {
-    tema: 'Operacional',
-    pergunta: 'Qual a porcentagem de vendas se concentra nos marketplaces?',
-    alternativas: [
-      { texto: '78', valor: true },
-      { texto: '95', valor: false },
-      { texto: '70', valor: false },
-      { texto: '80', valor: false },
-    ],
-  },
-  {
-    tema: 'Varejo',
-    pergunta: 'A cada 10 brasileiros, quantos compram pela internet?',
-    alternativas: [
-      { texto: '7', valor: true },
-      { texto: '9', valor: false },
-      { texto: '5', valor: false },
-      { texto: '3', valor: false },
-    ],
-  },
-  {
-    tema: 'Logística',
-    pergunta:
-      'Dos brasileiros que compram online qual a porcentagem dos que utilizam os Marketplaces?',
-    alternativas: [
-      { texto: '95%', valor: true },
-      { texto: '30%', valor: false },
-      { texto: '85%', valor: false },
-      { texto: '60%', valor: false },
-    ],
-  },
-  {
-    tema: 'Operacional',
-    pergunta:
-      'É importante ter uma estratégia comercial para ser competitivo vendendo on-line? ',
+    tema: 'Inova 1',
+    pergunta: 'Qual definição melhor representa uma startup?',
     alternativas: [
       {
-        texto:
-          'Sim, você precisará trabalhar com margens pequenas e a estratégia comercial te ajudará a definir quais produtos devem ser colocados em promoção e em que circunstâncias isso ocorrerá',
+        texto: 'Empresa com modelo de negócio inovador, repetível e escalável',
         valor: true,
       },
       {
         texto:
-          'Não, vender on-line é mais simples e não necessita de uma estratégia bem formulada.',
+          'Show de humor executado por apenas um comediante, que se apresenta geralmente em pé',
         valor: false,
       },
+      { texto: 'Empresa com menos de 100 funcionários', valor: false },
+    ],
+  },
+  {
+    tema: 'Inova 1',
+    pergunta: 'O que é um unicórnio no contexto das startups?',
+    alternativas: [
+      { texto: 'Startup avaliada em US$ 1 bilhão ou mais', valor: true },
+      { texto: 'Startup criada no Vale do Silício', valor: false },
       {
         texto:
-          'Sim, para vendas on-line é necessário sair do que esta em alta e investir em produtos alternativos, com pouca competitividade.',
-        valor: false,
-      },
-      {
-        texto:
-          'Sim, você precisará focar em preços mais baixos que o do mercado, apenas assim você conseguirá relevância.',
+          'Animal mitológico que tem a forma de um cavalo, geralmente branco, com um único chifre em espiral',
         valor: false,
       },
     ],
   },
   {
-    tema: 'Varejo',
-    pergunta: '16.	Conquistar boas avaliações é importante porque:',
+    tema: 'Inova 1',
+    pergunta: 'O que faz um investidor-anjo?',
     alternativas: [
       {
         texto:
-          'Após observar preço, condição de pagamento e frete, os compradores tendem a consultar a opinião de outras pessoas para saber a experiência delas tanto no ato da compra quanto com o uso do produto.',
+          'Investe em uma startup financeiramente e também a apoio com conhecimento',
         valor: true,
       },
-      {
-        texto: 'Seu negócio fica mais conhecido pelos seus clientes',
-        valor: false,
-      },
-      {
-        texto:
-          'Possibilita vender produtos de menor qualidade por um preço mais algo, já que as avaliações são boas.',
-        valor: false,
-      },
-      { texto: 'Nenhuma das alternativas ', valor: false },
+      { texto: 'Investidor já falecido', valor: false },
+      { texto: 'Investe e atua no dia a dia de uma startup', valor: false },
     ],
   },
   {
-    tema: 'Varejo',
-    pergunta: 'Cuidar da reputação de sua loja é importante?  ',
+    tema: 'Inova 1',
+    pergunta: 'Qual é o principal objetivo da inovação?',
+    alternativas: [
+      {
+        texto: 'Criar produtos e serviços mais eficientes e rentáveis',
+        valor: true,
+      },
+      { texto: 'Manter o status quo e evitar mudanças', valor: false },
+      {
+        texto: 'Reduzir a criatividade e a diversidade no mercado',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 1',
+    pergunta: 'O que é um MVP (Minimum Viable Product)?',
     alternativas: [
       {
         texto:
-          'Sim, pois isso é um fator avaliado por vários compradores na hora de decidir de quem comprará, além de ser um fator considerado pelo algoritmo para definir quais os anúncios são mais relevantes para o cliente e serão mostrados primeiro.',
+          'Uma versão básica de um produto lançada no mercado para validar sua viabilidade',
         valor: true,
       },
       {
         texto:
-          'Sim, é a partir da reputação que você consegue cobrar preços mais altos que o do mercado, aumento seu lucro.',
+          'Um produto completo e finalizado, pronto para ser comercializado',
+        valor: false,
+      },
+      {
+        texto: 'Um protótipo experimental usado apenas para fins de pesquisa',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 1',
+    pergunta: 'O que é Bussiness Model Canvas?',
+    alternativas: [
+      { texto: 'Canvas de Modelo de Negócio', valor: true },
+      { texto: 'Personograma', valor: false },
+      { texto: 'Mapa de Empatia', valor: false },
+    ],
+  },
+  {
+    tema: 'Inova 1',
+    pergunta: 'Qual é o papel da prototipagem rápida no processo de inovação?',
+    alternativas: [
+      {
+        texto: 'Testar ideias e conceitos de forma rápida e econômica',
+        valor: true,
+      },
+      {
+        texto: 'Atrasar o processo de inovação e aumentar os custos',
         valor: false,
       },
       {
         texto:
-          'Não, tendo bons preços e bons produtos, de nada vale a reputação.',
+          'Evitar a experimentação e avançar diretamente para a produção em massa',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 1',
+    pergunta: 'O que significa a sigla IoT?',
+    alternativas: [
+      {
+        texto: 'Internet of Things (Internet das Coisas)',
+        valor: true,
+      },
+      {
+        texto: 'Internet of Thoughts (Internet dos Pensamentos)',
         valor: false,
       },
       {
-        texto: 'Não, a única utilidade é de status para a loja.',
+        texto: 'Internet of Teams (Internet das Equipes)',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 1',
+    pergunta: 'O que é design thinking?',
+    alternativas: [
+      {
+        texto:
+          'Uma abordagem centrada no ser humano para resolver problemas complexos',
+        valor: true,
+      },
+      {
+        texto: 'Um método para evitar qualquer tipo de pensamento criativo',
+        valor: false,
+      },
+      {
+        texto:
+          'Uma técnica usada exclusivamente para o design de produtos físicos',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 1',
+    pergunta:
+      'Qual é o principal benefício da cultura de inovação em uma organização?',
+    alternativas: [
+      {
+        texto: 'Estimular a criatividade e a geração contínua de novas ideias',
+        valor: true,
+      },
+      {
+        texto: 'Fomentar a resistência à mudança e ao crescimento',
+        valor: false,
+      },
+      {
+        texto: 'Limitar a diversidade de perspectivas e soluções',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'Onde fica o Vale do Silício?',
+    alternativas: [
+      {
+        texto: 'Califórnia (EUA)',
+        valor: true,
+      },
+      {
+        texto: 'Nova Iorque (EUA)',
+        valor: false,
+      },
+      {
+        texto: 'Governador Valadares (EUA)',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'Qual dessas startups não foi criada no Brasil? ',
+    alternativas: [
+      {
+        texto: 'Rappi',
+        valor: true,
+      },
+      {
+        texto: '99',
+        valor: false,
+      },
+      {
+        texto: 'Meliuz',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta:
+      'Qual a importância do apoio do SEBRAE e de entidades, como o Parque Tecnológico no Desenvolvimento de Negócios Inovadores?',
+    alternativas: [
+      {
+        texto:
+          'São importantes pois apoiam o desenvolvimento das startups com conhecimento (cursos, palestras, etc.), conexões, mentorias, apoio estrutural, rodadas de investimento, etc.        ',
+        valor: true,
+      },
+      {
+        texto: 'Fornecem linhas de crédito somente',
+        valor: false,
+      },
+      {
+        texto: 'Permite terceirizar o desenvolvimento do meu negócio',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'Qual é o papel da inteligência artificial na inovação?',
+    alternativas: [
+      {
+        texto:
+          'Automatizar tarefas repetitivas e ajudar a identificar padrões ocultos',
+        valor: true,
+      },
+      {
+        texto: 'Limitar o progresso tecnológico e inibir a criatividade',
+        valor: false,
+      },
+      {
+        texto: 'Substituir completamente os humanos na geração de ideias',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'O que é um "early adopter"?',
+    alternativas: [
+      {
+        texto:
+          'Um indivíduo que adota uma nova tecnologia ou produto logo no início',
+        valor: true,
+      },
+      {
+        texto: 'Alguém que evita qualquer tipo de mudança ou inovação',
+        valor: false,
+      },
+      {
+        texto:
+          'Um termo utilizado para descrever pessoas resistentes a novidades',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'O que é open innovation?',
+    alternativas: [
+      {
+        texto:
+          'A prática de colaborar e buscar soluções externas para desafios internos',
+        valor: true,
+      },
+      {
+        texto:
+          'O isolamento completo de uma organização, sem interação com o mundo exterior',
+        valor: false,
+      },
+      {
+        texto:
+          'A promoção de ideias restritas apenas a um grupo seleto de indivíduos',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'O que é pivotar?',
+    alternativas: [
+      {
+        texto: 'Uma mudança estratégica de direção em um projeto ou negócio',
+        valor: true,
+      },
+      {
+        texto:
+          'A estratégia de nunca mudar os planos, independentemente das circunstâncias',
+        valor: false,
+      },
+      {
+        texto: 'A exclusão completa de todas as ideias inovadoras',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'O que é o pensamento lateral?',
+    alternativas: [
+      {
+        texto:
+          'Uma abordagem criativa para resolver problemas por meio de perspectivas não convencionais',
+        valor: true,
+      },
+      {
+        texto:
+          'Um estilo de pensamento limitado às formas tradicionais de solução de problemas',
+        valor: false,
+      },
+      {
+        texto:
+          'Um método de análise estruturada usado exclusivamente na área da matemática',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'O que é um disruptor?',
+    alternativas: [
+      {
+        texto:
+          'Uma inovação que causa uma mudança significativa em um setor ou mercado',
+        valor: true,
+      },
+      {
+        texto: 'Uma estratégia para manter o status quo e evitar perturbações',
+        valor: false,
+      },
+      {
+        texto: 'Uma pessoa que se opõe a todas as formas de inovação',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 2',
+    pergunta: 'O que é a prototipagem iterativa?',
+    alternativas: [
+      {
+        texto:
+          'O processo de criar protótipos e refiná-los com base no feedback contínuo',
+        valor: true,
+      },
+      {
+        texto:
+          'A produção de um único protótipo final sem oportunidade para melhorias',
+        valor: false,
+      },
+      {
+        texto: 'A rejeição completa de qualquer forma de prototipagem',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'Qual foi o primeiro unicórnio brasileiro?',
+    alternativas: [
+      {
+        texto: '99',
+        valor: true,
+      },
+      {
+        texto: 'NUBANK',
+        valor: false,
+      },
+      {
+        texto: 'Cavalo do Beto Carreiro',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'Quem é Jeff Bezos?',
+    alternativas: [
+      {
+        texto: 'Fundador e CEO da Amazon',
+        valor: true,
+      },
+      {
+        texto: 'Fundador e CEO do Spotify',
+        valor: false,
+      },
+      {
+        texto: 'Fundador e CEO do Google',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'O que faz um investidor-anjo?',
+    alternativas: [
+      {
+        texto:
+          'Investe em uma startup financeiramente e também a apoio com conhecimento',
+        valor: true,
+      },
+      {
+        texto: 'Investidor já falecido',
+        valor: false,
+      },
+      {
+        texto: 'Investe e atua no dia a dia de uma startup',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'O que é Pitch?',
+    alternativas: [
+      {
+        texto:
+          'Apresentação do negócio para despertar interesse de possíveis investidores',
+        valor: true,
+      },
+      {
+        texto: 'É um meio de pagamento',
+        valor: false,
+      },
+      {
+        texto: 'Cantora de rock brasileira',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'O que é o pensamento criativo?',
+    alternativas: [
+      {
+        texto: 'A habilidade de gerar novas ideias e soluções originais',
+        valor: true,
+      },
+      {
+        texto: 'A tendência de evitar qualquer forma de criatividade',
+        valor: false,
+      },
+      {
+        texto: 'A crença de que a inovação é desnecessária no mundo moderno',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'O que é um brainstorming?',
+    alternativas: [
+      {
+        texto:
+          'Uma técnica para gerar um grande número de ideias de forma rápida e colaborativa',
+        valor: true,
+      },
+      {
+        texto:
+          'A exclusão completa de todas as ideias que não se encaixam nas normas existentes',
+        valor: false,
+      },
+      {
+        texto:
+          'Um método para evitar qualquer tipo de discussão ou colaboração',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'O que é a curva de adoção de inovação?',
+    alternativas: [
+      {
+        texto:
+          'Uma representação gráfica que mostra a taxa de aceitação de uma nova tecnologia ou produto ao longo do tempo',
+        valor: true,
+      },
+      {
+        texto:
+          'Um modelo que desencoraja a adoção de qualquer tipo de inovação',
+        valor: false,
+      },
+      {
+        texto: 'Uma ferramenta usada apenas para promover a conformidade',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'O que é um hub de inovação?',
+    alternativas: [
+      {
+        texto:
+          'Um centro de colaboração e intercâmbio de conhecimento entre empresas e indivíduos',
+        valor: true,
+      },
+      {
+        texto: 'Um lugar onde todas as formas de inovação são proibidas',
+        valor: false,
+      },
+      {
+        texto:
+          'Um dispositivo físico usado exclusivamente para armazenar ideias inovadoras',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta: 'Quais são os benefícios da inovação em pequenos negócios?',
+    alternativas: [
+      {
+        texto:
+          'Aumento da competitividade, criação de novas oportunidades de negócios e melhoria da eficiência',
+        valor: true,
+      },
+      {
+        texto:
+          'Aumento dos custos operacionais, redução da flexibilidade e diminuição do alcance de mercado',
+        valor: false,
+      },
+      {
+        texto:
+          'Aumento das barreiras de entrada, redução da colaboração e estagnação do crescimento',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Inova 3',
+    pergunta:
+      'Quais são os principais desafios enfrentados pelos pequenos negócios ao buscar a inovação?',
+    alternativas: [
+      {
+        texto:
+          'Restrição de recursos, falta de conhecimento especializado e resistência à mudança',
+        valor: true,
+      },
+      {
+        texto:
+          'Facilidade de acesso a financiamentos, excesso de concorrência e falta de visão de longo prazo',
+        valor: false,
+      },
+      {
+        texto:
+          'Sobrecarga de burocracia, baixa demanda por produtos/serviços inovadores e falta de infraestrutura adequada',
         valor: false,
       },
     ],
